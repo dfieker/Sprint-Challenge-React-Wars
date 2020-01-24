2d12d796bb84dc56c5ce4a9b621c27bfeab28813
Daniel Fieker <danielmfieker@gmail.com>
2020-01-24 16:06:44 -0600

    MVP - finishing styled-components

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,16 +1,18 @@
 import React from "react";
 import CharacterPost from "./CharacterPost";
 
+
 const Characters = props => {
     console.log("Characters", props);
     return (
         <div className="character-list">
-            {props.characterData.results.map(element => {
+            {props.characterData.map(element => {
+              return (
           <CharacterPost
             key={element.height}
             name={element.name}
             hairColor={element.hair_color}
-          />
+          />)
             })}
             {/* <h2>Character: {props.name}</h2>
             <p>Hair Color: {props.hairColor}</p> */}
